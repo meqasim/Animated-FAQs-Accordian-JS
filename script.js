d7128d1b294d3791980be31c16c9f2eedc9f5593
HaFiZ CH QaSiM <63259297+meqasim@users.noreply.github.com>
2024-05-26 17:53:28 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 const accordianJS = document.querySelectorAll('accordian');
+
+accordianJS.forEach(accordian => {
+    const icon = accordian.querySelector('.icon')
+    const answer = accordian.querySelector('.answer')
+
+    accordian.addEventListener('click', () => {
+        icon.classList.toggle('active')
+        answer.classList.toggle('active')
+    })
+})
